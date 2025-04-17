ed0a4db031f21f93746eb3784ef01f8bb32a1319
Geraldine Derroire <geraldine.derroire@cirad.fr>
2025-04-17 11:43:29 -0300

    add tidyr in tidyverse

--- 4_tidyverse/script/create_dataset.R
+++ 4_tidyverse/script/create_dataset.R
@@ -1,5 +1,7 @@
 # This script transform existing dataset ou create new ones to be used in the sessions on the tidyverse
 
+library(tidyverse)
+
 
 ### Data set of traits from rcontroll
 rcontroll::data(TROLLv3_species)
@@ -11,3 +13,36 @@ traits <- TROLLv3_species %>%
 
 write_csv(traits, "4_tidyverse/data/func_traits.csv")
 
+
+
+# Small datasets for the second session with messy data
+###########################################################
+
+# for tidyr ex 1
+dt1 <- tibble(
+  tree = c("tree1", "tree2", "tree3"),
+  `2015` = c(25, 35, 43),
+  `2020` = c(26, 37, 43),
+  `2025` = c(26.5, 38.5, 43),
+)
+
+# for tidyr ex 2
+dt2 <- tibble(
+  taxo = c("Anacardiaceae_Myracrodruon_urundeuva", # Aroeira
+          "Fabaceae_Hymenaea_martiana", # Jatoba-da-mata
+          "Favaceae_Piptadenia_macradenia"), # Pau-jacaré
+  DBH = c("10, 12, 21" , "13", "24, 18")
+)
+
+# for janitor
+dt3 <- tibble(
+  sítio = c("A", NA, NA),
+  `% cobertura` = c(0.25, NA, NA),
+  numParcela = c(1, NA, NA))
+
+
+## Save in a single .Rdata
+save(dt1,
+     dt2,
+     dt3,
+     file = "4_tidyverse/data/data_tidyverse_2.RData")
